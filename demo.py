318622839b12cdfc4bcd44a3dfd004f7fb3021ed
2sakthi4 <sakthiveln2408@gmail.com>
2025-04-09 15:50:07 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+# Python3 program to add two numbers
+num1 = int(input("enter the number"))
+num2 = int(input("enter the number"))
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print(sum) 
